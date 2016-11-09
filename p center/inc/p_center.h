9cb8ff30a5ca98addf6123b29e196a798f84ce9c
zhjc <735828661@qq.com>
2016-11-09 22:58:37 +0800

    add pcenter project related files

---
+++ p center/inc/p_center.h
@@ -0,0 +1,11 @@
+
+#ifndef _HO_P_CENTER_H
+#define _HO_P_CENTER_H
+
+#include "config.h"
+
+HO_NAMESPACE_BEGIN(pcenter)
+
+HO_NAMESPACE_END
+
+#endif
---
+++ p center/src/p_center.cpp
@@ -0,0 +1,6 @@
+
+#include "p_center.h"
+
+HO_NAMESPACE_BEGIN(pcenter)
+
+HO_NAMESPACE_END
